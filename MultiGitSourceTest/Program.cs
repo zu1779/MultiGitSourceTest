e77c8da8e455e8404692c181e1934c04ed850ea8
Mauro Dalla Zuanna <maurodallazuanna@hotmail.com>
2018-07-14 13:12:06 +0200

    Initial commit.

--- MultiGitSourceTest/Program.cs
+++ MultiGitSourceTest/Program.cs
@@ -7,6 +7,9 @@ namespace MultiGitSourceTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
